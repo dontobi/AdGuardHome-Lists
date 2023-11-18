cbaa4dce77e2d4d675f6b2b916b805d875ad3a86
Tobias 'dontobi' Schug <github@myhome.zone>
2023-11-18 17:30:00 +0100

    Update build_blacklist.sh

--- build_blacklist.sh
+++ build_blacklist.sh
@@ -2,9 +2,11 @@
 
 # Download blacklists
 wget -q -O raw_dontobi.tmp https://raw.githubusercontent.com/dontobi/AdGuardHome-Lists/main/blacklist.txt
-wget -q -O raw_hagezi_1.tmp https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.plus.txt
+wget -q -O raw_hagezi_1.tmp https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt
 wget -q -O raw_hagezi_2.tmp https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/tif.txt
-wget -q -O raw_hagezi_3.tmp https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/spam-tlds.txt
+wget -q -O raw_hagezi_3.tmp https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/hoster.txt
+wget -q -O raw_hagezi_4.tmp https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/spam-tlds.txt
+wget -q -O raw_hagezi_5.tmp https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/gambling.txt
 wget -q -O raw_elliotwutingfeng.tmp https://raw.githubusercontent.com/elliotwutingfeng/Inversion-DNSBL-Blocklists/main/Google_hostnames_light_ABP.txt
 
 # Prepair blacklists
@@ -12,6 +14,8 @@ sed -e "/^||/!d" raw_dontobi.tmp > bl_dontobi.tmp
 sed -e "/^||/!d" raw_hagezi_1.tmp > bl_hagezi_1.tmp
 sed -e "/^||/!d" raw_hagezi_2.tmp > bl_hagezi_2.tmp
 sed -e "/^||/!d" raw_hagezi_3.tmp > bl_hagezi_3.tmp
+sed -e "/^||/!d" raw_hagezi_4.tmp > bl_hagezi_4.tmp
+sed -e "/^||/!d" raw_hagezi_5.tmp > bl_hagezi_5.tmp
 sed -e "/^||/!d" -e 's/$all/^/' raw_elliotwutingfeng.tmp > bl_elliotwutingfeng.tmp
 
 # combine lists
